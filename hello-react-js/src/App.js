651b1a15db87cdf6984751e32637205a868124db
Muhammad Atif <m.atifwebdeveloper@gmail.com>
2023-08-09 19:00:44 +0500

    start read

---
+++ hello-react-js/src/App.js
@@ -0,0 +1,14 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hello React JS by Atif
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
