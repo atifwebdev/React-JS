9af06ccb09d2d1b614f191a9539b60604b08fe73
Muhammad Atif <m.atifwebdeveloper@gmail.com>
2023-08-15 18:45:20 +0500

    create new resume

---
+++ my-resume/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function Hi() {
+    return <div>Hello <strong> Atif This is my Resume</strong></div>;
+}
+
+ReactDOM.render(<Hi />, document.querySelector('#root'));
